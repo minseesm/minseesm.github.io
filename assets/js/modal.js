98ec20488b9babbaad4f08a9cd5692bcd43aaf44
MiNew_Dev <devminew@MiNew-Devui-MacBookAir.local>
2023-10-22 22:11:50 -0700

    Popup Fixed

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,6 +1,9 @@
 document.querySelectorAll('.snap-open').forEach(element => {
     element.addEventListener('click', function() {
         document.querySelector('.snap-modal').classList.add('open')
+        setTimeout(()=> {
+            document.querySelector('.snap-modal').classList.add('open-delay')
+        }, 200)
         document.body.classList.add("modal-active");
         window.location.hash = 'snap'
 
@@ -10,6 +13,9 @@ document.querySelectorAll('.snap-open').forEach(element => {
 document.querySelectorAll('.wedding-open').forEach(element => {
     element.addEventListener('click', function() {
         document.querySelector('.wedding-modal').classList.add('open')
+        setTimeout(()=> {
+            document.querySelector('.wedding-modal').classList.add('open-delay')
+        }, 200)
         document.body.classList.add("modal-active");
         window.location.hash = 'wedding'
 
@@ -19,6 +25,9 @@ document.querySelectorAll('.wedding-open').forEach(element => {
 document.querySelectorAll('.package-open').forEach(element => {
     element.addEventListener('click', function() {
         document.querySelector('.package-modal').classList.add('open')
+        setTimeout(()=> {
+            document.querySelector('.package-modal').classList.add('open-delay')
+        }, 200)
         document.body.classList.add("modal-active");
         window.location.hash = 'package'
 
@@ -28,6 +37,9 @@ document.querySelectorAll('.package-open').forEach(element => {
 document.querySelectorAll('.video-open').forEach(element => {
     element.addEventListener('click', function() {
         document.querySelector('.video-modal').classList.add('open')
+        setTimeout(()=> {
+            document.querySelector('.video-modal').classList.add('open-delay')
+        }, 200)
         document.body.classList.add("modal-active");
         window.location.hash = 'video'
         document.querySelector('#background-video-monitor').pause();
@@ -42,6 +54,7 @@ document.querySelectorAll('.close-modal').forEach(element => {
         document.body.classList.remove("modal-active");
         document.querySelectorAll('.modal-container').forEach(modal => {
             modal.classList.remove('open')
+            modal.classList.remove('open-delay')
             history.replaceState("", "", location.pathname)
             window.location.hash = '';
         })
@@ -55,17 +68,29 @@ document.addEventListener('DOMContentLoaded', function() {
         const hash =  window.location.hash.replace('#', '')
         if(hash === 'snap') {
             document.querySelector('.snap-modal').classList.add('open')
+            setTimeout(()=> {
+                document.querySelector('.snap-modal').classList.add('open-delay')
+            }, 200)
             document.body.classList.add("modal-active");
         } else if (hash === 'wedding') {
             document.body.classList.add("modal-active");
             document.querySelector('.wedding-modal').classList.add('open')
+            setTimeout(()=> {
+                document.querySelector('.wedding-modal').classList.add('open-delay')
+            }, 200)
         } else if (hash === 'video') {
             document.body.classList.add("modal-active");
             document.querySelector('.video-modal').classList.add('open')
+            setTimeout(()=> {
+                document.querySelector('.video-modal').classList.add('open-delay')
+            }, 200)
             document.querySelector('#video-modal-player').pause();
         } else if (hash === 'package') {
             document.body.classList.add("modal-active");
             document.querySelector('.package-modal').classList.add('open')
+            setTimeout(()=> {
+                document.querySelector('.package-modal').classList.add('open-delay')
+            }, 200)
         } else {
             document.querySelector('#background-video-monitor').play()
         }
